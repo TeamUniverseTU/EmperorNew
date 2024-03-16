58dd138e6cc163946cfc58ff1ef45e7e89637b79
TeamUniversTU <138324953+TeamUniverseTU@users.noreply.github.com>
2024-03-16 09:10:15 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -10,7 +10,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b>╔════════════⦿\n├⋗ ᴄʀᴇᴀᴛᴏʀ : <a href='tg://user?id={5493260414}'>⚚ 𝐓𝐑𝐈𝐏𝐏𝐘 ❄️ </a>\n├⋗ ʟᴀɴɢᴜᴀɢᴇ : <code>Python3</code>\n├⋗ ʟɪʙʀᴀʀʏ : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n├⋗ ꜱᴏᴜʀᴄᴇ ᴄᴏᴅᴇ : <a href=https://t.me/Trippy_Xt>File Store Bot</a>\n├⋗ Main Channel : <a href=https://t.me/Animes_Xyz>Anime Xyz</a>\n├⋗ Support Group : <a href=https://t.me/Animetalks0>Anime Talks</a>\n╚═════════════════⦿</b>",
+            text = f"<b>╔════════════⦿\n├⋗ ᴏᴡɴᴇʀ : <a href='tg://user?id={2098973647}'>This Person</a>\n\n </a>\n├⋗ Help : <a href='https://t.me/AryanTeamUniverse/'>Aryan Via Owner </a>\n├⋗ Credits : <a href=https://t.me/Trippy_xt>𝐓𝐑𝐈𝐏𝐏𝐘</a>\n├⋗ Main Channel : <a href=https://t.me/teamUniverseOffical>TeamUniverse</a>\n├⋗ Support Group : <a href=https://t.me/AnimeUniverse_Chatt>Group Chat</a>\n╚═════════════════⦿</b>",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
