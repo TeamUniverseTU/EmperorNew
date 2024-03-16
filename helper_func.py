575b4137dd983e0154c68a0a423f6ca7d4e48563
Kakashi Hatake <138500712+Kakashi069@users.noreply.github.com>
2024-03-16 07:32:36 +0530

    Update helper_func.py

--- helper_func.py
+++ helper_func.py
@@ -8,6 +8,10 @@ from pyrogram.enums import ChatMemberStatus
 from config import FORCE_SUB_CHANNEL, FORCE_SUB_CHANNEL2, FORCE_SUB_CHANNEL3, FORCE_SUB_CHANNEL4, ADMINS
 from pyrogram.errors.exceptions.bad_request_400 import UserNotParticipant
 from pyrogram.errors import FloodWait
+from config import JOIN_REQS_DB
+from database.join_reqs import JoinReqs
+
+db = JoinReqs
 
 async def is_subscribed(filter, client, update):
     if not FORCE_SUB_CHANNEL:
@@ -57,14 +61,17 @@ async def is_subscribed(filter, client, update):
     else:
         return True
 
-async def is_subscribed(filter, client, update):
-    if not FORCE_SUB_CHANNEL3:
+async def is_subscribed4(filter, client, update):
+    if not FORCE_SUB_CHANNEL4:
         return True
-    user_id = update.from_user.id
+    user_id = update.from_user.id    
+    user = await db().get_user(update.from_user.id)
     if user_id in ADMINS:
         return True
+    if user and user["user_id"] == update.from_user.id:
+        return True
     try:
-        member = await client.get_chat_member(chat_id = FORCE_SUB_CHANNEL3, user_id = user_id)
+        member = await client.get_chat_member(chat_id = FORCE_SUB_CHANNEL4, user_id = user_id)
     except UserNotParticipant:
         return False
 
@@ -158,4 +165,5 @@ def get_readable_time(seconds: int) -> str:
 
 
 subscribed = filters.create(is_subscribed)
+subscribed4 = filters.create(is_subscribed4)
        
