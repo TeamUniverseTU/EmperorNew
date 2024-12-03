ae76207044ba4b2f58048c60c4c38db7612af6ef
TeamUniversTU <138324953+TeamUniverseTU@users.noreply.github.com>
2024-12-03 20:26:44 +0530

    Update config.py

--- config.py
+++ config.py
@@ -11,7 +11,7 @@ from logging.handlers import RotatingFileHandler
 
 
 #Bot token @Botfather
-TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "7017158354:AAGoFgu6LMpoxoU8g5Epp-l9JzkfVm8EtNY")
+TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "7017158354:AAGO2B-lxQcjJ0js27icBFh8KWFiSf6MMrQ")
 
 #Your API ID from my.telegram.org
 APP_ID = int(os.environ.get("APP_ID", "22418774"))
@@ -26,7 +26,7 @@ CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1001882054182"))
 OWNER_ID = int(os.environ.get("OWNER_ID", "2098973647"))
 
 #Port
-PORT = os.environ.get("PORT", "8030")
+PORT = os.environ.get("PORT", "6969")
 
 #Database
 DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://UniverseHUb:AryanDamddyTU@universehub.kqpwqml.mongodb.net/?retryWrites=true&w=majority&appName=UniverseHUb")
